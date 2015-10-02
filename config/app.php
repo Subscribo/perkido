4dffb6a587d8f305ae48f1b0892cbc0a1fdf45db
Michael G. Ströck michael@stroeck.com <michael@stroeck.com>
2015-10-02 10:11:31 +0200

    Install Laravel. HAML funcitonality for Blade templates.

--- config/app.php
+++ config/app.php
@@ -145,6 +145,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Third Party Service Providers
+         */
+        'trupedia\LaravelBladeHaml\LaravelBladeHamlServiceProvider',
+
     ],
 
     /*
